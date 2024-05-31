7b0a5bc0a1a42a8743aa455967900e91f5b2d6f0
Windsmith <shriramsekar11@gmail.com>
2024-05-31 11:47:31 +0400

    feat: write function to hash a password

--- Bloomfilter.py
+++ Bloomfilter.py
@@ -1,10 +1,22 @@
+import hashlib
+
 class Bloomfilter:
     def __init__(self):
-        self.size = [5000]
+        self.size = 1000
         self.num_of_hashes = 15
         
-    def hash_functions(self, password):
-        return password
+    def hash(self, password: str):
+        hashes = []
+        password = password.encode()
+        for i in range(self.num_of_hashes):
+            g = hashlib.md5()
+            h = hashlib.sha256()
+            g.update(password)
+            h.update(password)
+             
+            hash = (int(g.hexdigest(), base=16) + i * int(h.hexdigest(), base=16)) % self.size
+            hashes.append(hash)
+        return hashes
     
     def train_filter(self,file):
         file = open('rockyou.txt', 'r')
@@ -13,3 +25,7 @@ class Bloomfilter:
     def test(self,newpassword):
         return True
     
+#Testing
+a = Bloomfilter()
+b = input("Enter string: ")
+print(a.hash(b))
